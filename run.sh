3652fe3bb850b9b7b88d2e595e6e55e70850f4e8
devansh-dvj <devansh.dvj@gmail.com>
2025-01-25 23:22:18 -0600

    Updated run script

--- run.sh
+++ run.sh
@@ -2,8 +2,26 @@
 
 make DEBUG=0
 
-if [[ -f cs_ssa ]]; then
-    echo "Compilation Done!"
+if [[ -f cos_ssa ]]; then
+    # CFG Tests
+    ./cos_ssa --tool=cfg tests/0.cfg
+    ./cos_ssa --tool=cfg tests/1.cfg
+    ./cos_ssa --tool=cfg tests/2.cfg
+    ./cos_ssa --tool=cfg tests/3.cfg
+    ./cos_ssa --tool=cfg tests/4.cfg
+    ./cos_ssa --tool=cfg tests/5.cfg
+    ./cos_ssa --tool=cfg tests/6.cfg
+    ./cos_ssa --tool=cfg tests/7_orig.cfg
+    ./cos_ssa --tool=cfg tests/7_dupl.cfg
+    ./cos_ssa --tool=cfg tests/8.cfg
+    ./cos_ssa --tool=cfg tests/9.cfg
+    ./cos_ssa --tool=cfg tests/10.cfg
+
+    # SSA Tests
+    ./cos_ssa --tool=ssa tests/0.ssa
+    ./cos_ssa --tool=ssa tests/6.ssa
+    ./cos_ssa --tool=ssa tests/7_orig.ssa
+    ./cos_ssa --tool=ssa tests/7_exp.ssa
 else
     echo "Compilation Failed!"
 fi
