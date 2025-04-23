aeb58bd18d7afe1ac5f6c86ca3672cc2bfa0ca9b
devansh-dvj <devansh.dvj@gmail.com>
2025-04-22 23:18:46 -0500

    Updated run script

--- run.sh
+++ run.sh
@@ -1,27 +1,26 @@
 #/bin/sh
 
-make DEBUG=0
-
-if [[ -f cos_ssa ]]; then
+# Run the tests
+if [[ -f ./build/cos_ssa ]]; then
     # CFG Tests
-    ./cos_ssa --tool=cfg tests/0.cfg
-    ./cos_ssa --tool=cfg tests/1.cfg
-    ./cos_ssa --tool=cfg tests/2.cfg
-    ./cos_ssa --tool=cfg tests/3.cfg
-    ./cos_ssa --tool=cfg tests/4.cfg
-    ./cos_ssa --tool=cfg tests/5.cfg
-    ./cos_ssa --tool=cfg tests/6.cfg
-    ./cos_ssa --tool=cfg tests/7_orig.cfg
-    ./cos_ssa --tool=cfg tests/7_dupl.cfg
-    ./cos_ssa --tool=cfg tests/8.cfg
-    ./cos_ssa --tool=cfg tests/9.cfg
-    ./cos_ssa --tool=cfg tests/10.cfg
+    ./build/cos_ssa --tool=cfg tests/0.cfg
+    ./build/cos_ssa --tool=cfg tests/1.cfg
+    ./build/cos_ssa --tool=cfg tests/2.cfg
+    ./build/cos_ssa --tool=cfg tests/3.cfg
+    ./build/cos_ssa --tool=cfg tests/4.cfg
+    ./build/cos_ssa --tool=cfg tests/5.cfg
+    ./build/cos_ssa --tool=cfg tests/6.cfg
+    ./build/cos_ssa --tool=cfg tests/7_orig.cfg
+    ./build/cos_ssa --tool=cfg tests/7_dupl.cfg
+    ./build/cos_ssa --tool=cfg tests/8.cfg
+    ./build/cos_ssa --tool=cfg tests/9.cfg
+    ./build/cos_ssa --tool=cfg tests/10.cfg
 
     # SSA Tests
-    ./cos_ssa --tool=ssa tests/0.ssa
-    ./cos_ssa --tool=ssa tests/6.ssa
-    ./cos_ssa --tool=ssa tests/7_orig.ssa
-    ./cos_ssa --tool=ssa tests/7_exp.ssa
+    ./build/cos_ssa --tool=ssa tests/0.ssa
+    ./build/cos_ssa --tool=ssa tests/6.ssa
+    ./build/cos_ssa --tool=ssa tests/7_orig.ssa
+    ./build/cos_ssa --tool=ssa tests/7_exp.ssa
 else
     echo "Compilation Failed!"
 fi
