c7a919a18f16b5112bd1c83e0cb08da8a2649454
devansh-dvj <devansh.dvj@gmail.com>
2025-04-23 23:34:21 -0500

    Added clean build

--- build.sh
+++ build.sh
@@ -1,7 +1,21 @@
 #/bin/sh
 
-mkdir -p build && cd build
-cmake .. -DBUILD_DEBUG=OFF
+# If the script is run with the argument "clean", it will remove the build directory.
+if [[ $1 == "clean" ]]; then
+    echo "Cleaning build directory..."
+    rm -rf build
+fi
+
+# Check if the build directory exists, build only new changes
+if [[ -d "build" ]]; then
+    echo "Build directory exists. Rebuiding only for new changes..."
+    cd build
+else
+    echo "Build directory does not exist. Creating..."
+    mkdir -p build && cd build
+    cmake .. -DBUILD_DEBUG=OFF
+fi
+
 cmake --build . -j4
 if [[ $? -ne 0 ]]; then
     echo "Build failed!"
