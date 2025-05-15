ebcb69ad4e95b8f7dbb5a1bfae5d386a0d72bf44
devansh-dvj <devansh.dvj@gmail.com>
2025-05-15 10:10:30 -0500

    Updated eval script to start from .c files

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+module load llvm/15.0.7
+
 # Set the working directory
 cd "$(dirname "$0")"/..
 ulimit -Sv 8388608
@@ -7,6 +9,14 @@ ulimit -t 60
 
 # Run the tests
 if [[ -f ./build/cos_ssa ]]; then
+    clang -S -emit-llvm -o benchmarks/dds7.ll benchmarks/dds7.c
+    clang -S -emit-llvm -o benchmarks/dijkstra_large.ll benchmarks/dijkstra_large.c
+    clang++ -S -emit-llvm -o benchmarks/enchantedmaze.ll benchmarks/enchantedmaze.cpp
+    clang -S -emit-llvm -o benchmarks/int-stc.ll benchmarks/int-stc.c
+    clang -S -emit-llvm -o benchmarks/int-stc2.ll benchmarks/int-stc2.c
+    clang++ -S -emit-llvm -o benchmarks/int-stcp.ll benchmarks/int-stcp.cpp
+    clang -S -emit-llvm -o benchmarks/shell.ll benchmarks/shell.c
+
     /usr/bin/time -v ./build/cos_ssa --tool=all benchmarks/dds7.ll
     /usr/bin/time -v ./build/cos_ssa --tool=all benchmarks/dijkstra_large.ll
     /usr/bin/time -v ./build/cos_ssa --tool=all benchmarks/enchantedmaze.ll
